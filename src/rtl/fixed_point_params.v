7ec3bf8d4d7dd5adb480040e51dc4ac15d1f4a60
Anton Maurovic <anton@foogle.com>
2023-09-05 17:42:24 +0930

    (test010) Try Q8.10 instead of Q9.10

--- src/rtl/fixed_point_params.v
+++ src/rtl/fixed_point_params.v
@@ -8,7 +8,7 @@
 // It seems the smaller the player step size, the bigger Qm needs to be. Non-power-of-2 steps could make this worse.
 // For instance, with Q12.12, it seems the smallest reliable step quantum is 8, i.e. 8*(2^-12) => 0.001953125.
 // This might be made better if we properly check for reciprocal saturation.
-`define Qm          9                   // Signed.
+`define Qm          8                   // Signed. 8 is minimum, else rayAddend overflows.
 `define Qn          10                  // Currently 9 seems to be the lowest value for clean 640x480, but 10+ is recommended.
 `define Qmn         (`Qm+`Qn)
 `define QMI         (`Qm-1)             // Just for convenience; M-1.
