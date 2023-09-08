6c66b7cde18f3af211994c000cb04fbad974ab84
Anton Maurovic <anton@foogle.com>
2023-09-08 10:57:35 +0930

    Adding PicoDeo interface for SPI

--- de0nano/raybox-zero_de0nano.v
+++ de0nano/raybox-zero_de0nano.v
@@ -89,14 +89,53 @@ module raybox_zero_de0nano(
   assign g = (gg==2'b11) ? 1'b1 : (gg==2'b10) ? dither_hi : (gg==2'b01) ? dither_lo : 1'b0;
   assign b = (bb==2'b11) ? 1'b1 : (bb==2'b10) ? dither_hi : (bb==2'b01) ? dither_lo : 1'b0;
 
+  // Pico to DE0-Nano GPIO mapping: https://github.com/algofoogle/journal/blob/master/0094-2023-06-12.md#pin-mapping-chart
+  wire [29:0] pico_gpio = {
+    1'b0,       // 29 - NC
+    gpio0[ 9],  // 28
+    gpio0[13],  // 27
+    gpio0[15],  // 26
+    1'b0,       // 25 - NC
+    1'b0,       // 24 - NC
+    1'b0,       // 23 - NC
+    gpio0[19],  // 22
+    gpio0[23],  // 21
+    gpio0[11],  // 20
+    gpio0[25],  // 19
+    gpio0[27],  // 18
+    gpio0[31],  // 17
+    gpio0[33],  // 16
+    gpio0[32],  // 15
+    gpio0[30],  // 14
+    gpio0[26],  // 13
+    gpio0[24],  // 12
+    gpio0[28],  // 11
+    gpio0[22],  // 10
+    gpio0[18],  //  9
+    gpio0[16],  //  8
+    gpio0[14],  //  7
+    gpio0[12],  //  6
+    gpio0[ 8],  //  5
+    gpio0[20],  //  4
+    gpio0[ 6],  //  3
+    gpio0[ 4],  //  2
+    gpio0[10],  //  1
+    gpio0[ 2],  //  0
+  };
+
+  // These are our unsynchronised inputs (i.e. different clock domain):
+  wire i_sclk = pico_gpio[28];
+  wire i_mosi = pico_gpio[27];
+  wire i_ss_n = pico_gpio[26];
+
   rbzero game(
     // --- Inputs: ---
     .clk        (clock_25),
     .reset      (reset),
-    // // SPI:
-    // .i_sclk(),
-    // .i_mosi(),
-    // .i_ss_n(),
+    // SPI:
+    .i_sclk     (i_sclk),
+    .i_mosi     (i_mosi),
+    .i_ss_n     (i_ss_n),
 
     // --- Outputs: ---
     .hsync_n    (hsync),
