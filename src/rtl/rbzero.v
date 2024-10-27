7444c68287926bd82f7bc1edac7a2d938d27e884
Anton Maurovic <anton@foogle.com>
2024-10-27 19:03:03 +1030

    Comment out optional defines: Target can choose instead

--- src/rtl/rbzero.v
+++ src/rtl/rbzero.v
@@ -4,14 +4,14 @@
 // `include "fixed_point_params.v"
 // `include "helpers.v"
 
-//SMELL: These should probably be defined by the target (e.g. TT04 or FPGA) rather than inline here:
-`define USE_MAP_OVERLAY
-`define USE_DEBUG_OVERLAY
+// These can be defined by the target (e.g. TT04 or FPGA) rather than inline here:
+//`define USE_MAP_OVERLAY
+//`define USE_DEBUG_OVERLAY
 //`define TRACE_STATE_DEBUG  // Trace state is represented visually per each line on-screen.
 //`define STANDBY_RESET // If defined use extra logic to avoid clocking regs during reset (for power saving/stability).
-`define RESET_TEXTURE_MEMORY // Should there be an explicit reset for local texture memory?
-`define RESET_TEXTURE_MEMORY_PATTERNED // If defined with RESET_TEXTURE_MEMORY, texture memory reset is a pattern instead of black.
-`define DEBUG_NO_TEXTURE_LOAD // If defined, prevent texture loading
+//`define RESET_TEXTURE_MEMORY // Should there be an explicit reset for local texture memory?
+//`define RESET_TEXTURE_MEMORY_PATTERNED // If defined with RESET_TEXTURE_MEMORY, texture memory reset is a pattern instead of black.
+//`define DEBUG_NO_TEXTURE_LOAD // If defined, prevent texture loading
 
 module rbzero(
   input               clk,
